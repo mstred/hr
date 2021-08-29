0db60d37ec4e0c9154793ba59a7196b5b9c8984d
Edson Samuel Jr <samuedson@gmail.com>
2021-08-29 14:51:59 -0300

    Create Python package structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='UTF-8') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='hr',
+    version='0.1.0',
+    description='Command line user export utility',
+    long_description=readme,
+    author='Edson Samuel Jr',
+    email='samuedson@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+)
